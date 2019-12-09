1f53a4fffd586eee9a31ff1c892012a1b73c969e
tyhobbs <tjhobbs1@dmacc.edu>
2019-12-08 22:26:23 -0600

    correction to link made

--- account/views.py
+++ account/views.py
@@ -16,7 +16,7 @@ def registration_view(request):
 			raw_password = form.cleaned_data.get('password1')
 			account = authenticate(email=email, password=raw_password)
 			login(request, account)
-			return redirect('detail')
+			return redirect('dashboard')
 		else:
 			context['registration_form'] = form
 
